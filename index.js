c447a2cc90dc0692bfdde7e9d5cf79ebf82bce31
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-08-16 21:56:43 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+const ImageStore = require('./lib/imageStore');
+let db;
+
+var multer = require('multer');
+var upload = multer();
+
+// let images = [
+//     {src: 'animal.jpg'},
+//     {src: 'winter-hunting-dog.jpg'},
+//     {src: 'cat-crossed-legs.jpg'},
+//     {src: 'Labrador_Chocolate.jpg'},
+//     {src: 'purple-pattern.jpg'},
+//     {src: 'Wreath_icon.png'},
+// ];
+// {src: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Creative-Tail-Animal-dog.svg'}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var logRequests = function (req, res, next) {
+    console.log(`${req.method} @ ${req.url}`);
+    next();
+}
+
+app.use(logRequests);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/images', (req, res) => {
+    imageStore.findImages((images) => {
+        res.send(images);
+    });
+});
+
+app.post('/images', upload.array(), (req, res) => {
+    imageStore.addImage(req.body, () => {
+        res.redirect('/');
+    });
+});
+
+let db_uri = process.env.MONGO_URL || 'mongodb://localhost:27017';
+
+MongoClient.connect(db_uri, { useNewUrlParser: true }, (error, client) => {
+
+    if (error) return console.error('Error connecting to MongoDB: ', error);
+
+    db = client.db('mural');
+    imageStore = new ImageStore(db);
+
+    app.listen((process.env.PORT || 3000), () => {
+        console.log('Listening on port ' + 3000);
+    });
+});
