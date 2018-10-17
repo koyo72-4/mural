01f784f65f357f92a0b6872c5a1355fc428ad296
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-10-17 00:56:49 -0400

    moved mural image up

--- seed/images.js
+++ seed/images.js
@@ -7,33 +7,63 @@ module.exports = [
     },
 
     {
-       src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeoAG1hCyQkfLC8z2t9H0ZeeW03peN9GSrfEqCzyYpdnRNb5kuDQ',
-       borderColor: 'rgba(255, 0, 0, 1)',
-       createdAt: new Date('September 6, 2018 12:31:05:123')
-    },
-
-    {
-        src: 'https://upload.wikimedia.org/wikipedia/commons/2/27/Black_white_cats.jpg',
-        borderColor: 'rgba(0, 0, 255, 1)',
-        createdAt: new Date('September 6, 2018 12:32:05:123')
-    },
-
-    {
-        src: 'https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg?cs=srgb&dl=animal-dog-maltese-33053.jpg&fm=jpg',
-        borderColor: 'rgba(0, 255, 0, 1)',
-        createdAt: new Date('September 6, 2018 12:33:05:123')
-    },
-    
-    {
-        src: 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        borderColor: 'rgba(0, 0, 255, 1)',
-        createdAt: new Date('September 6, 2018 12:34:05:123')
-    },
-
-    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/CoCo3system.jpg',
         borderColor: 'rgba(255, 0, 0, 1)',
         createdAt: new Date('September 6, 2018 12:35:05:123')
+    },
+
+    {
+        src: 'https://images.pexels.com/photos/163007/phone-old-year-built-1955-bakelite-163007.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        borderColor: 'rgba(0, 0, 255, 1)',
+        createdAt: new Date('September 6, 2018 12:40:05:123')
+    },
+
+    {
+        src: 'https://www.publicdomainpictures.net/pictures/210000/nahled/raumschiff-1480689858Yem.jpg',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 12:45:05:123')
+    },
+
+    {
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUA7j-sY3u1mEgvWMvV5YdTulBE58dZ4BRGN03ltJ4jjzA1kPVcQ',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 12:50:05:123')
+    },
+
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Roadsign_-_Cellphone.svg/2000px-Roadsign_-_Cellphone.svg.png',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 12:55:05:123')
+    },
+
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/SoftBank_pepper.JPG',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 13:00:05:123')
+    },
+
+    {
+        src: 'https://www.publicdomainpictures.net/pictures/140000/nahled/pinball.jpg',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 13:05:05:123')
+    }, 
+
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Synthesizers.com_Modular.jpg',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 13:10:05:123')
+    }, 
+
+    {
+        src: 'https://www.publicdomainpictures.net/pictures/30000/nahled/atm-machine.jpg',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 13:15:05:123')
+    }, 
+
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Cardinal_Cuckoo_Clock%2C_126_1st_Ave._Minneapolis_MN.jpg',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        createdAt: new Date('September 6, 2018 13:20:05:123')
     }
 
 ];
